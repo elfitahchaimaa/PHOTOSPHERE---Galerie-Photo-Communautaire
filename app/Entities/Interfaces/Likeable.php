1a669bd521c4a2290aed24d7cda5d9f165cd7c87
elfitah <elfitahchaimaa@gmail.com>
2026-01-06 10:33:37 +0100

    creer des interfaces

---
+++ app/Entities/Interfaces/Likeable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Interfaces;
+
+interface Likeable
+{
+    public function addLike(int $userId): bool;
+
+    public function removeLike(int $userId): bool;
+
+    public function isLikedBy(int $userId): bool;
+
+    public function getLikeCount(): int;
+
+    public function getLikedBy(): array;
+}
